1f52f4a45f9708504b3e9ba7d888e9866b6088dd
NicholasCH <nicholasch96@gmail.com>
2022-03-31 17:47:15 -0500

    root folder name change

--- config/connection.js
+++ config/connection.js
@@ -7,9 +7,12 @@ const sequelize = process.env.JAWSDB_URL
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: 'localhost',
       dialect: 'mysql',
+      port: /*process.env.PORT ||*/ 3306,
       dialectOptions: {
         decimalNumbers: true,
       },
     });
+
+    
 
 module.exports = sequelize;
